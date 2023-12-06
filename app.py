7a84995eb8dcf361348b3f04199a2e9df4ee4caa
kapilesh-ap <151802457+kapilesh-ap@users.noreply.github.com>
2023-12-06 12:43:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import joblib
 import streamlit as st
 from xgboost import XGBClassifier
 import os
+from sklearn.preprocessing import StandardScaler
 
 # Header
 st.write("""
@@ -68,16 +69,16 @@ genhlth = col3.selectbox("6. General Health: How would you rank your General Hea
                          ('Excellent', 'Very Good', 'Good', 'Fair', 'Poor'), index=3)
 
 # 7. HeartDiseaseorAttack
-HeartDiseaseorAttack = col4.selectbox("7. : Do you have coronary heart disease?",
+HeartDiseaseorAttack = col4.selectbox("7. Do you have coronary heart disease?",
                          ('Yes', 'No'), index=1)
 
 # 8. PhysActivity
-PhysActivity = col5.selectbox("8. : Do you have any physical activity in past 30 days - not including job?",
-                         ('Yes', 'No'), index=1)
+PhysActivity = col5.selectbox("8. Now thinking about your physical health, which includes physical illness and injury, for how many days during the past 30 days was your physical health not good?",
+                         (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30), index=3)
 
 
 # 9. PhysActivity
-DiffWalk = col6.selectbox("9. : Do you have serious difficulty walking or climbing stairs?",
+DiffWalk = col6.selectbox("9. Do you have serious difficulty walking or climbing stairs?",
                          ('Yes', 'No'), index=1)
 
 
@@ -118,10 +119,9 @@ def prep_df(df):
 
     # Drop Weight and Height
     df = df.drop(columns=['Weight', 'Height'])
-
+    df['PhysHlth'] = df['PhysActivity']
     # Re-Order columns
-    df = df[['BMI', 'Age', 'HighChol', 'HighBP', 'GenHlth','HeartDiseaseorAttack','PhysActivity','DiffWalk']]
-
+    df = df[['HighBP','HighChol', 'BMI', 'GenHlth', 'DiffWalk', 'Age', 'HeartDiseaseorAttack', 'PhysHlth']]
     # Age
     df['Age'] = df['Age'].replace({'Age 18 to 24': 1, 'Age 25 to 29': 2, 'Age 30 to 34': 3, 'Age 35 to 39': 4, 'Age 40 to 44': 5, 'Age 45 to 49': 6,
                                    'Age 50 to 54': 7, 'Age 55 to 59': 8, 'Age 60 to 64': 9, 'Age 65 to 69': 10, 'Age 70 to 74': 11, 'Age 75 to 79': 12, 'Age 80 or older': 13})
@@ -135,9 +135,7 @@ def prep_df(df):
     # HeartDiseaseorAttack
     df['HeartDiseaseorAttack'] = df['HeartDiseaseorAttack'].replace({'Yes': 1, 'No': 0})
 
-    # PhysActivity
-    df['PhysActivity'] = df['PhysActivity'].replace({'Yes': 1, 'No': 0})
-    
+
     # DiffWalk
     df['DiffWalk'] = df['DiffWalk'].replace({'Yes': 1, 'No': 0})
 
@@ -161,8 +159,9 @@ model_path = os.path.join(script_dir, 'dt_model.pkl')
 
 # Load the model
 model = joblib.load(model_path)
-prediction = model.predict(df)
-prediction_probability = model.predict_proba(df)
+df_scaled=model['scaler'].transform(df)
+prediction = model['model'].predict(df_scaled)
+prediction_probability = model['model'].predict_proba(df)
 low_risk_proba = round(prediction_probability[0][0] * 100)
 high_risk_proba = round(prediction_probability[0][1] * 100)
 
